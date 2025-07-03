bff6bdc3a6e1d4a0e9b20fc01cefabff640e04ef
Gibranbintang <gibranbintang2004@gmail.com>
2025-07-03 17:14:33 +0700

    Update fungsi.php

--- WEB TI/fungsi.php
+++ WEB TI/fungsi.php
@@ -1,1 +1,24 @@
+<?php
+
+ $koneksi = mysqli_connect("localhost:3307","root","","webti");
+
+    if(!$koneksi)
+    {
+        die("Gagal".mysqli_connect_error($koneksi));
+    }
+
+    function ambildata($query)
+    {
+        global $koneksi;
+
+        $result=mysqli_query($koneksi,$query);
+
+        $rows = [];
+
+        while($row = mysqli_fetch_assoc($result))
+        {
+            $rows[] = $row;
+        }
+         return $rows;
+    }
 
